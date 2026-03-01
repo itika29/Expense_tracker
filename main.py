3a39a8b93e5d8b84f7b9887741ed4a95ee707dbf
Itika Kansal <kansaliti07@gmail.com>
2026-03-01 17:18:44 +0530

    Version 1 - Clean dictionary based expense tracker

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from fundamentalfunc import date,custom_category,display,remain_budget ;
 
 budget = int (input("Enter your budget  :"))
-# date_in= input("Enter date (dd-mm-yyyy): ") = 0 
-date()
-print("📌Enter your Expenses! \nHow much do you spent on: ")
-# Food = int(input("Food : "))
-#     Stationary = int(input( "Stationary :"))
-#     Travel = int(input( "Travel :"))
-#     Miscellaneous = int(input( "Miscellaneous :"))
 
+date()
+
+print("📌Enter your Expenses! \nHow much do you spent on: ")
 match = {"Food":0, "Travel":0 , "Stationary":0 , "Miscellaneous":0}
 
 for key in match:
     x = int(input(f"{key} : "))
     match[key] = x
-    # Stationary = int(input( "Stationary :"))
-    # Travel = int(input( "Travel :"))
-    # Miscellaneous = int(input( "Miscellaneous :"))
+
 custom_category(match)
 display(match)
 remain_budget(budget,match)
